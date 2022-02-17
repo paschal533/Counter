bb8cb252ad4cfdd9988f1294ca048c73bbef6907
unknown <okwuosahpaschal@gmail.com>
2022-02-17 23:26:20 +0100

    second commit

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 
 const getWeb3 = () =>
-  new Promise((resolve, reject) => {
+  //new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener("load", async () => {
       // Modern dapp browsers...
