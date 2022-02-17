d4e4130f81981a8bab715b699a4c1d5434144dc4
unknown <okwuosahpaschal@gmail.com>
2022-02-17 23:26:51 +0100

    second commit

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -1,12 +1,12 @@
 import Web3 from "web3";
 
 const getWeb3 = () =>
-  //new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener("load", async () => {
       // Modern dapp browsers...
       if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
+        //const web3 = new Web3(window.ethereum);
         try {
           // Request account access if needed
           await window.ethereum.enable();
